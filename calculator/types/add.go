c7587168684dd52bfc9f786a8a87a2bb0d961642
Mohammad Khaledi <mohammad.khaledi@acquia.com>
2019-08-21 11:48:03 -0400

    Fix things back to struct instead of duplicate types

--- calculator/types/add.go
+++ calculator/types/add.go
@@ -2,12 +2,14 @@ package types
 
 import "github.com/looking-4-justice/calculator/interfaces"
 
-type Add  BinaryOperator
+type Add struct {
+	BinaryOperator
+}
 
 func (bo Add) Evaluate() float64 {
 	return bo.Left.Evaluate() + bo.Right.Evaluate()
 }
 
 func NewAdd(left interfaces.Expression, right interfaces.Expression) Add {
-	return Add{Left: left, Right: right, Operator: '+'}
+	return Add{BinaryOperator{Left: left, Right: right, Operator: '+'}}
 }
--- calculator/types/multiply.go
+++ calculator/types/multiply.go
@@ -2,12 +2,14 @@ package types
 
 import "github.com/looking-4-justice/calculator/interfaces"
 
-type Multiply BinaryOperator
+type Multiply struct {
+	BinaryOperator
+}
 
 func (bo Multiply) Evaluate() float64 {
 	return bo.Left.Evaluate() * bo.Right.Evaluate()
 }
 
 func NewMultiply(left interfaces.Expression, right interfaces.Expression) Multiply {
-	return Multiply{Left: left, Right: right, Operator: '*'}
+	return Multiply{BinaryOperator{Left: left, Right: right, Operator: '*'}}
 }
